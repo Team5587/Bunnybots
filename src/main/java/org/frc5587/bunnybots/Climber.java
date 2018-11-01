c61882641753e2058ca68f08fd9af36c7a590126
aiden2904 <34355375+aiden2904@users.noreply.github.com>
2018-11-01 13:43:06 -0400

    Update src/main/java/org/frc5587/bunnybots/Climber.java

--- src/main/java/org/frc5587/bunnybots/Climber.java
+++ src/main/java/org/frc5587/bunnybots/Climber.java
@@ -8,13 +8,13 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Climber extends Subsystem{
 
     private TalonSRX climberTalon = new TalonSRX(9);
-    
+
     public Climber(){
     }
 
     public void climberUp() {
         climberTalon.set(ControlMode.PercentOutput, 1);
-    }
+    } 
 
     @Override
     protected void initDefaultCommand() {
