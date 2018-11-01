cd2d7aff2e1409346ec3112afd8aa9a0620473a1
aiden2904 <34355375+aiden2904@users.noreply.github.com>
2018-11-01 10:54:30 -0400

    Added Grabber command stuff

--- src/main/java/org/frc5587/bunnybots/Grabbers.java
+++ src/main/java/org/frc5587/bunnybots/Grabbers.java
@@ -1,5 +1,42 @@
-package org.frc5587.bucmdnnybots;
+package org.frc5587.bunnybots;
 
-public class Grabbers {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Grabbers extends Subsystem{
+
+    public Grabbers(){
+
+    }
+
+    private DoubleSolenoid expanderSolenoid = new DoubleSolenoid(1, 0);
+    private TalonSRX leftTalon = new TalonSRX(6), rightTalon = new TalonSRX(5);
+    private XboxController controller = new XboxController(0);
+
+    public void grabberOpen(){
+        expanderSolenoid.set(DoubleSolenoid.Value.kForward);
+    }
+
+    public void grabberClose(){
+        expanderSolenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+
+    public void grabberIn(){
+        leftTalon.set(ControlMode.PercentOutput, 1);
+        rightTalon.set(ControlMode.PercentOutput, -1);
+    }
+
+    public void grabberOut(){
+        leftTalon.set(ControlMode.PercentOutput, -1);
+        rightTalon.set(ControlMode.PercentOutput, 1);
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
 }
