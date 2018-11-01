0d6c3473946969f5896f9f647a8df09b17a8297c
caroline w <cwinakur@icloud.com>
2018-11-01 11:00:37 -0400

    add joystick
    in OI

--- src/main/java/org/frc5587/bunnybots/OI.java
+++ src/main/java/org/frc5587/bunnybots/OI.java
@@ -4,5 +4,6 @@ import edu.wpi.first.wpilibj.XboxController;
 
 public class OI {
     public static XboxController controller = new XboxController(0);
+    public static Joystick joystick = new Joystick(1);
 
 }
