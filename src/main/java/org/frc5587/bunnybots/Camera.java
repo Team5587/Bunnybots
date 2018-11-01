de662bd16f4e60fba8c11f180dddfcd43f697332
aiden2904 <34355375+aiden2904@users.noreply.github.com>
2018-11-01 11:23:34 -0400

    Camera Code

--- src/main/java/org/frc5587/bunnybots/Camera.java
+++ src/main/java/org/frc5587/bunnybots/Camera.java
@@ -1,11 +1,10 @@
 package org.frc5587.bunnybots;
 
+import edu.wpi.first.wpilibj.CameraServer;
 
 public class Camera {
-    // public static final int DRIVER_CAMERA = 0;
-    // public static final int GRABBER_CAMERA = 1;
-    private Camera driverCamera = new Camera(0);
-    private Camera grabberCamera = new Camera(1);
+    private CameraServer driverCamera = new Camera(2);
+    private CameraServer grabberCamera = new Camera(3);
 
     public Camera() {
     }
