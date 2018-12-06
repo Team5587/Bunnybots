ca7592313336b3debc4d3219e7f7c457364efddb
caroline w <cwinakur@icloud.com>
2018-12-05 22:48:09 -0500

    deleted irrelevant comment

--- src/main/java/org/frc5587/bunnybots/ControlDoor.java
+++ src/main/java/org/frc5587/bunnybots/ControlDoor.java
@@ -11,10 +11,6 @@ public class ControlDoor extends Command {
     @Override
     protected void execute() {
 
-        /*
-         * won't this actually be controlled based on the input from the system that
-         * determines the colour of the balls in the intake?
-         */
         if (OI.controller.getAButtonPressed()) {
             Robot.door.doorClose();
         } else if (OI.controller.getYButtonPressed()) {
