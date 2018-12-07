3ecc5d8385a05811fe3f5c424fdf435042e83236
aiden2904 <34355375+aiden2904@users.noreply.github.com>
2018-12-07 09:51:56 -0500

    Added open/close angles, added close door on start

--- src/main/java/org/frc5587/bunnybots/ControlDoor.java
+++ src/main/java/org/frc5587/bunnybots/ControlDoor.java
@@ -3,18 +3,25 @@ package org.frc5587.bunnybots;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class ControlDoor extends Command {
+    private boolean open = false;
 
     public ControlDoor() {
+        requires(Robot.door);
+    }
 
+    @Override
+    public synchronized void start() {
+        Robot.door.doorClose();
     }
 
     @Override
     protected void execute() {
-
-        if (OI.controller.getAButtonPressed()) {
+        if (OI.controller.getAButtonPressed() && open) {
             Robot.door.doorClose();
-        } else if (OI.controller.getBButtonPressed()) {
+            open = false;
+        } else if (OI.controller.getAButtonPressed() && !open) {
             Robot.door.doorOpen();
+            open = true;
         }
     }
 
