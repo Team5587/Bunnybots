550f1edbe2c1af39d7913db594fd27f5c3e220a9
chw9 <43727884+chw9@users.noreply.github.com>
2018-12-12 15:36:44 -0500

    fixed talon can id

--- src/main/java/org/frc5587/bunnybots/RobotMap.java
+++ src/main/java/org/frc5587/bunnybots/RobotMap.java
@@ -5,7 +5,7 @@ public class RobotMap {
 
     public static class Claw {
         public static final int[] PISTON_CLAW = { 1, 2 };
-        public static final int TALON = 3;
+        public static final int TALON = 5;
     }
 
     public static class DriveTrain {
